c7108f2ba6ef1dedcf6c04049d99a030f8c629c7
InJun <123285589+NotARealDoct0r@users.noreply.github.com>
2025-03-12 22:59:35 -0400

    Modified (vue.config.js) so that it doesn't overwrite (_base_vue.html) each time vue is loaded, added a browser tab icon + logo (nav bar), added CSS, & created home + contact us pages (app)

--- vue-games/vue.config.js
+++ vue-games/vue.config.js
@@ -3,9 +3,9 @@
 //   transpileDependencies: true
 // })
 module.exports = {
-  publicPath: 'http://localhost:8080', // The base URL where your app will be deployed 
-  outputDir: '../static/dist', // The path for where files will be output when the app is built
-  indexPath: '../../templates/_base_vue.html', // The path for the generated index file
+  publicPath: '/', // Change this to '/' so it works in both dev & production
+  outputDir: '../static/dist', // Keep built files in static folder
+  indexPath: '../static/dist/index.html', // Store Vue’s index.html in static, NOT templates!
 
   configureWebpack: {
     devServer: {
