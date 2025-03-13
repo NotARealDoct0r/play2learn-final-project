94b6bdb1dc16035408b9ddd4ca29b2ce919c7309
InJun <123285589+NotARealDoct0r@users.noreply.github.com>
2025-03-12 23:47:37 -0400

    created a Review model then completed migrations to add the model to the database

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Review(models.Model):
+    message = models.TextField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.message
