dec033dc7e22ee88ba3dbe189dd02146ca52f6d0
InJun <123285589+NotARealDoct0r@users.noreply.github.com>
2025-03-15 00:57:10 -0400

    added Delete feature

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from .views import (ReviewCreateView ,ReviewDetailView, ReviewListView, 
-                    ReviewUpdateView)
+from .views import (
+    ReviewCreateView, ReviewDeleteView,ReviewDetailView, ReviewListView, ReviewUpdateView
+)
 
 app_name = 'reviews'
 urlpatterns = [
     path('review/<int:pk>/update/', ReviewUpdateView.as_view(), name='update'),
+    path('review/<int:pk>/delete/', ReviewDeleteView.as_view(), name='delete'),
     path('review/create/', ReviewCreateView.as_view(), name='create'),
     path('review/<int:pk>/', ReviewDetailView.as_view(), name='detail'),
     path('', ReviewListView.as_view(), name='list'),
--- reviews/views.py
+++ reviews/views.py
@@ -1,10 +1,18 @@
-from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from django.urls import reverse_lazy
+
+from django.views.generic import (
+    CreateView, DeleteView,DetailView, ListView, UpdateView
+)
 
 from .models import Review
 
 class ReviewCreateView(CreateView):
     model = Review
     fields = ['title', 'message']
+
+class ReviewDeleteView(DeleteView):
+    model = Review
+    success_url = reverse_lazy('reviews:list')
 
 class ReviewDetailView(DetailView):
     model = Review
