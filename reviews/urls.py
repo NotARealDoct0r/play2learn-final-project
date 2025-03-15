6a6dd23465d36cc5f299a71dc6f4dc3b5b9dc976
InJun <123285589+NotARealDoct0r@users.noreply.github.com>
2025-03-15 00:51:07 -0400

    added abilities to create and update reviews

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import ReviewDetailView, ReviewListView
+from .views import (ReviewCreateView ,ReviewDetailView, ReviewListView, 
+                    ReviewUpdateView)
 
 app_name = 'reviews'
 urlpatterns = [
+    path('review/<int:pk>/update/', ReviewUpdateView.as_view(), name='update'),
+    path('review/create/', ReviewCreateView.as_view(), name='create'),
     path('review/<int:pk>/', ReviewDetailView.as_view(), name='detail'),
     path('', ReviewListView.as_view(), name='list'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,9 +1,17 @@
-from django.views.generic import DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
 
 from .models import Review
+
+class ReviewCreateView(CreateView):
+    model = Review
+    fields = ['title', 'message']
 
 class ReviewDetailView(DetailView):
     model = Review
 
 class ReviewListView(ListView):
     model = Review
+
+class ReviewUpdateView(UpdateView):
+    model = Review
+    fields = ['title', 'message']
