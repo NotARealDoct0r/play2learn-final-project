16e9bcdc49be4211dfec131764911436d9fee1f8
InJun <123285589+NotARealDoct0r@users.noreply.github.com>
2025-03-15 00:34:25 -0400

    added title field to Review model + completed migration

--- reviews/models.py
+++ reviews/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 
 class Review(models.Model):
+    title = models.TextField(max_length=100)
     message = models.TextField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
