cd598bd5811f26070b325a843b3b6b07ae42ffbe
InJun <123285589+NotARealDoct0r@users.noreply.github.com>
2025-03-16 21:58:38 -0400

    added the Review Table to Django Admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Review
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    model = Review
+
+    # List attributes
+    list_display = ['title', 'created', 'updated']
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj: # editing an existing object
+            return ('created', 'updated')
+
+        return ()
