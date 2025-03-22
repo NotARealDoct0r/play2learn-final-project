9613bb93674a8092656d0e249d0764c3b989c423
InJun <123285589+NotARealDoct0r@users.noreply.github.com>
2025-03-22 01:21:54 -0400

    created a my account page + password change page

--- play2learn/urls.py
+++ play2learn/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/', include('users.urls')),
     path('account/', include('allauth.urls')),
     path('reviews/', include('reviews.urls')),
     path('contact-us/', include('contact_us.urls')),
