9716c1babdd8c9b646aae30367a1ebafda3c5389
InJun <123285589+NotARealDoct0r@users.noreply.github.com>
2025-03-23 22:00:04 -0400

    added Bootstrap carousel to rotate reviews in a slide show on the homepage

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,14 @@
 from django.views.generic import TemplateView
 
+from reviews.models import Review
+
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["review_list"] = Review.objects.all()  # Add review data
+        return context
 
 class ContactUsView(TemplateView):
     template_name = 'pages/contact_us.html'
